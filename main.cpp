137517c6fab8cc1d0b73e6b3e62249217207e824
Kielx <kiel66@gmail.com>
2024-03-12 23:34:19 +0100

    Add base for SFML game project

--- main.cpp
+++ main.cpp
@@ -2,9 +2,10 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
+    sf::RenderWindow window(sf::VideoMode(1024, 768), "Arkanoid!");
+    sf::CircleShape shape(300.f);
     shape.setFillColor(sf::Color::Green);
+    shape.move(100, 100);
 
     while (window.isOpen())
     {
@@ -13,6 +14,31 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+            if(event.type == sf::Event::KeyPressed)
+            {
+				if (event.key.code == sf::Keyboard::Escape)
+					window.close();
+			}
+            if (event.type == sf::Event::KeyPressed)
+            {
+                if (event.key.code == sf::Keyboard::Left)
+                {
+					shape.move(-10, 0);
+
+				}
+                if (event.key.code == sf::Keyboard::Right)
+                {
+					shape.move(10, 0);
+				}
+                if (event.key.code == sf::Keyboard::Up)
+                {
+					shape.move(0, -10);
+				}
+                if (event.key.code == sf::Keyboard::Down)
+                {
+					shape.move(0, 10);
+				}
+			}
         }
 
         window.clear();
