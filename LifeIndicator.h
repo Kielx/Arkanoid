67bbf5240e4919f9c46f434de90a7ffda5836506
Kielx <kiel66@gmail.com>
2024-04-03 11:01:07 +0200

    Add LifeIndicator doxygen

--- LifeIndicator.h
+++ LifeIndicator.h
@@ -1,11 +1,29 @@
+/**
+ * @file LifeIndicator.h
+ * @brief This file contains the LifeIndicator class.
+ */
+
 #pragma once
 #include "Global.h"
 #include <SFML/Graphics.hpp>
+
+ /**
+  * @class LifeIndicator
+  * @brief This class represents a LifeIndicator in the game.
+  */
 class LifeIndicator
 {
-	public:
-	sf::RectangleShape lifeIndicatorShape;
+public:
+    /**
+     * @brief Represents the shape of the LifeIndicator.
+     */
+    sf::RectangleShape lifeIndicatorShape;
 
-	LifeIndicator(float posX, float posY, sf::Texture &texture);
+    /**
+     * @brief Constructor for the LifeIndicator class.
+     * @param posX The x-coordinate of the LifeIndicator.
+     * @param posY The y-coordinate of the LifeIndicator.
+     * @param texture The texture of the LifeIndicator.
+     */
+    LifeIndicator(float posX, float posY, sf::Texture& texture);
 };
-
