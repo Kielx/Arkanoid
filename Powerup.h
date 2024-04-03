4d250389c620293f72f440f5d53eeca4da332a32
Kielx <kiel66@gmail.com>
2024-04-03 11:05:46 +0200

    Update Powerup.h doxygen comments

--- Powerup.h
+++ Powerup.h
@@ -1,33 +1,106 @@
+/**
+ * @file Powerup.h
+ * @brief This file contains the Powerup class.
+ */
+
 #pragma once
 #include <SFML/Graphics.hpp>
 #include "Global.h"
 
+ /**
+  * @class Powerup
+  * @brief This class represents a Powerup in the game.
+  */
 class Powerup
 {
-	public:
-	float coinRadius{ 16.f };
-	sf::Vector2f position;
-	sf::Vector2f velocity = { 0.f, 2.f };
-	sf::Texture texture;
-	sf::CircleShape shape;
-	sf::Clock clock;
-	int powerupType = 0;
+public:
+    /**
+     * @brief The radius of the coin.
+     */
+    float coinRadius{ 16.f };
 
-	// This boolean value will be used to check
-	// whether powerup has been hit or not.
-	bool destroyed{ false };
+    /**
+     * @brief The position of the Powerup.
+     */
+    sf::Vector2f position;
 
+    /**
+     * @brief The velocity of the Powerup.
+     */
+    sf::Vector2f velocity = { 0.f, 2.f };
 
-	Powerup(float posX, float posY, int powerupType, sf::Texture &texture);
-	void update();
+    /**
+     * @brief The texture of the Powerup.
+     */
+    sf::Texture texture;
 
-	float x() { return shape.getPosition().x; }
-	float y() { return shape.getPosition().y; }
-	float left() { return x() - shape.getTextureRect().width / 2.f; }
-	float right() { return x() + shape.getTextureRect().width / 2.f; }
-	float top() { return y() - shape.getTextureRect().height / 2.f; }
-	float bottom() { return y() + shape.getTextureRect().height / 2.f;}
+    /**
+     * @brief Represents the shape of the Powerup.
+     */
+    sf::CircleShape shape;
 
+    /**
+     * @brief Clock to track time.
+     */
+    sf::Clock clock;
 
+    /**
+     * @brief The type of the Powerup.
+     */
+    int powerupType = 0;
+
+    /**
+     * @brief Represents whether the Powerup is destroyed or not.
+     */
+    bool destroyed{ false };
+
+    /**
+     * @brief Constructor for the Powerup class.
+     * @param posX The x-coordinate of the Powerup.
+     * @param posY The y-coordinate of the Powerup.
+     * @param powerupType The type of the Powerup.
+     * @param texture The texture of the Powerup.
+     */
+    Powerup(float posX, float posY, int powerupType, sf::Texture& texture);
+
+    /**
+     * @brief Updates the Powerup's state.
+     */
+    void update();
+
+    /**
+     * @brief Gets the x-coordinate of the Powerup.
+     * @return The x-coordinate of the Powerup.
+     */
+    float x() { return shape.getPosition().x; }
+
+    /**
+     * @brief Gets the y-coordinate of the Powerup.
+     * @return The y-coordinate of the Powerup.
+     */
+    float y() { return shape.getPosition().y; }
+
+    /**
+     * @brief Gets the left boundary of the Powerup.
+     * @return The left boundary of the Powerup.
+     */
+    float left() { return x() - shape.getTextureRect().width / 2.f; }
+
+    /**
+     * @brief Gets the right boundary of the Powerup.
+     * @return The right boundary of the Powerup.
+     */
+    float right() { return x() + shape.getTextureRect().width / 2.f; }
+
+    /**
+     * @brief Gets the top boundary of the Powerup.
+     * @return The top boundary of the Powerup.
+     */
+    float top() { return y() - shape.getTextureRect().height / 2.f; }
+
+    /**
+     * @brief Gets the bottom boundary of the Powerup.
+     * @return The bottom boundary of the Powerup.
+     */
+    float bottom() { return y() + shape.getTextureRect().height / 2.f; }
 };
-
